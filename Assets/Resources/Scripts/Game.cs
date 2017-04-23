a7aa7128661b25376193e0cb713887b8e358d3df
Jacky Chen <jacky.jjchen@gmail.com>
2017-04-24 01:59:13 +1000

    End to end

--- Assets/Resources/Scripts/Game.cs
+++ Assets/Resources/Scripts/Game.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 namespace jackyjjc {
 	public class Game : MonoBehaviour {
 		// ====================== All State Variables =====================
 		public GameObject screenDimming;
 		private GameState currentGameState;
+		private bool finishedInit;
 
 		// Variables for camera control
 		public float cameraZoomSpeed = 1;
@@ -38,7 +40,8 @@ namespace jackyjjc {
 		private static readonly string[] briefingTexts = new string[] {
 			"Our employer started sending migrants over to this planet even though we think this planet is too small. Sigh...I guess we have to prepare the landing of the first batch of the migrants." +
 			"\n\nGoal:\n" +
-			"- Successfully land at least 5 pods.",
+			"- Successfully land at least 5 pods." +
+			"- Have a score of 90 or more.",
 		};
 			
 		private int currentLevel;
@@ -47,12 +50,19 @@ namespace jackyjjc {
 			new Level1()
 		};
 
+		// ====================== endgame variables ========================
+		public GameObject endGameScreen;
+		private Text titleText;
+		private Text BodyText;
+		private Text finalScoreText;
+
 		// ====================== Gameplay variables ========================
 		public static readonly float PLANET_GRAVITY = 10f;
 		public static readonly float ROTATION_SPEED = 50f;
 		public static readonly float BLOCK_LANDING_PUSH = 5f;
 		public static readonly float PLANET_ROTATE_ANGLE_PER_SECOND = 0.8f;
 		public static readonly float MAX_ALLOWED_ANGLE = 20f;
+		public static readonly float MAX_ALLOWED_SIDEWAY_ANGLE = 40f;
 
 		private float blockDefaultFallingSpeed = 0.3f;
 		private float blockAccelerateSpeed = 5f;
@@ -76,27 +86,48 @@ namespace jackyjjc {
 
 		// Game stats
 		internal Dictionary<BlockType, int> numBlocksLanded;
+		private int _score;
+		internal int Score {
+			get { return _score; }
+			set { 
+				_score = value; 
+				this.scoreText.text = "Score: " + _score;
+			}
+		}
+		private BlockType upNext;
 
 		// UI
+		public GameObject popUpTextContainer;
 		private GameObject goalTextPrefab;
 		private GameObject goalListPanel;
 		private Text[] goalList;
+		private GameObject popUpTextPrefab;
+		private Text goalText;
+		private Text scoreText;
+		private GameObject infoPanel;
+		private Image upNextImage;
+		private Text upNextDescription;
+		private Text remainingText;
 
 		// Block types
-		private static readonly BlockType[] blockTypes = new BlockType[] {
-			new BlockType("foundation", 5, Color.red),
-			new BlockType("room", 2, Color.blue),
-			new BlockType("electriciy", 1, Color.yellow)
-		};
+		internal BlockType[] blockTypes;
 
 		void Start () {
 			// Initialise variables and load things
 			planetRadius = planet.GetComponent<CircleCollider2D> ().bounds.size.x / 2;
 			blockPrefab = Resources.Load<GameObject>("Prefabs/Block");
-			blockSize = blockPrefab.GetComponent<BoxCollider2D>().size.x;
+			blockSize = blockPrefab.GetComponent<BoxCollider2D>().size.x * 1.5f;
+
+			blockTypes = new BlockType[] {
+				new BlockType("Basic Pod (Blue)", 1, 10, new Color(27/255f, 98/255f, 1f), Resources.Load<Sprite>("Images/block_male"), ""),
+				new BlockType("Basic Pod (Pink)", 1, 10, new Color(1f, 85/255f, 85/255f), Resources.Load<Sprite>("Images/block_female"), "")
+			};
 
 			// Initialise all variables
+			this.finishedInit = false;
 			this.currentGameState = GameState.MAIN_MENU;
+			this.scoreText = this.gameplayUI.transform.FindChild ("GoalPanel/ScoreText").GetComponent<Text> ();
+			this.Score = 0;
 
 			// Init main menu state
 			this.titleScreen.SetActive (true);
@@ -116,26 +147,44 @@ namespace jackyjjc {
 			this.levelText = levelSwitchScreen.transform.FindChild ("LevelText").GetComponent<Text> ();
 			this.briefingText = levelSwitchScreen.transform.FindChild ("BriefingText").GetComponent<Text> ();
 
+			// Init end game state
+			this.endGameScreen.SetActive(false);
+			this.titleText = endGameScreen.transform.FindChild ("Title").GetComponent<Text> ();
+			this.finalScoreText = endGameScreen.transform.FindChild ("ScoreText").GetComponent<Text> ();
+			this.BodyText = endGameScreen.transform.FindChild ("Body").GetComponent<Text> ();
+
 			// Init gameplay state
 			this.gameplayUI.SetActive(false);
 			this.toBeRemove = new HashSet<GameObject> ();
-			currentSpawnHeight = blockSize * 4 + planetRadius;
+			currentSpawnHeight = blockSize * 3 + planetRadius;
 			cameraTargetSize = Camera.main.orthographicSize;
 			this.numBlocksLanded = new Dictionary<BlockType, int> ();
 			foreach (BlockType type in blockTypes) {
 				numBlocksLanded [type] = 0;
 			}
-			this.goalListPanel = this.gameplayUI.transform.FindChild("GoalPanel/").gameObject;
+			this.goalListPanel = this.gameplayUI.transform.FindChild("GoalPanel").gameObject;
 			this.goalTextPrefab = Resources.Load<GameObject> ("Prefabs/GoalText");
+			this.popUpTextPrefab = Resources.Load<GameObject> ("Prefabs/PopUpTextParent");
+			this.goalText = this.gameplayUI.transform.FindChild ("GoalPanel/GoalText").GetComponent<Text> ();
+			this.infoPanel = this.gameplayUI.transform.FindChild ("InfoPanel").gameObject;
+			this.upNextImage = this.infoPanel.transform.FindChild ("Panel/Image").GetComponent<Image> ();
+			this.upNextDescription = this.infoPanel.transform.FindChild ("Panel/Text").GetComponent<Text> ();
+			this.remainingText = this.infoPanel.transform.FindChild ("RemainingText").GetComponent<Text> ();
+
+			this.finishedInit = true;
 		}
 
 		void Update () {
-			if (currentGameState == GameState.INTRO) {
-				UpdateIntro ();
-			} else if (currentGameState == GameState.LEVEL_SWITCH) {
-				UpdateLevelSwitch ();
-			} else if (currentGameState == GameState.GAMEPLAY_STATE) {
-				UpdateGameplay ();
+			if (finishedInit) {
+				if (currentGameState == GameState.INTRO) {
+					UpdateIntro ();
+				} else if (currentGameState == GameState.LEVEL_SWITCH) {
+					UpdateLevelSwitch ();
+				} else if (currentGameState == GameState.GAMEPLAY_STATE) {
+					UpdateGameplay ();
+				} else if (currentGameState == GameState.GAME_END) {
+					UpdateEndGame ();
+				}
 			}
 
 			// Camera always updates
@@ -145,7 +194,7 @@ namespace jackyjjc {
 		}
 
 		void FixedUpdate() {
-			if (currentGameState == GameState.GAMEPLAY_STATE) {
+			if (finishedInit && (currentGameState == GameState.GAMEPLAY_STATE || currentGameState == GameState.GAME_END)) {
 				// Apply gravity to all landed objects and clean up fallen objects
 				Rigidbody2D[] children = landedBlocks.GetComponentsInChildren<Rigidbody2D>();
 				foreach(Rigidbody2D rigidBody in children) {
@@ -168,8 +217,7 @@ namespace jackyjjc {
 				}
 
 				foreach (var go in toBeRemove) {
-					numBlocksLanded [go.GetComponent<Block> ().blockType] -= 1;
-					Destroy (go);
+					DestroyBlock (go);
 				}
 				toBeRemove.Clear ();
 
@@ -189,7 +237,7 @@ namespace jackyjjc {
 					playerControlBlock.transform.position += gravityDirection * landingSpeed * Time.deltaTime;
 
 					// Apply rotation
-					playerControlBlock.transform.RotateAround(planet.transform.position, Vector3.forward, -1 * Input.GetAxis("Horizontal") * ROTATION_SPEED * Time.deltaTime);		
+					playerControlBlock.transform.RotateAround(planet.transform.position, Vector3.forward, -1 * Input.GetAxis("Horizontal") * ROTATION_SPEED * Time.deltaTime);
 				}
 
 				// Planet rotation 'animation' always happens
@@ -218,6 +266,7 @@ namespace jackyjjc {
 
 		// ============================ Intro ============================
 		private void StartIntro() {
+			finishedInit = false;
 			currentGameState = GameState.INTRO;
 			introScreen.SetActive (true);
 			Input.ResetInputAxes ();
@@ -226,6 +275,7 @@ namespace jackyjjc {
 			this.introDialogText.text = "";
 			this.cameraZoomSpeed = 0.2f;
 			this.skipPrompt.text = "Press 'S' to skip intro";
+			finishedInit = true;
 		}
 
 		private void UpdateIntro() {
@@ -263,34 +313,42 @@ namespace jackyjjc {
 
 		private void ExitIntro() {
 			introScreen.SetActive (false);
-			currentGameState = GameState.LEVEL_SWITCH;
 			this.cameraZoomSpeed = 1f;
 			StartLevelSwitch ();
 		}
 
 		// ============================ level Switch ==============================
 		private void StartLevelSwitch() {
+			this.finishedInit = false;
 			this.screenDimming.SetActive (true);
 			this.levelSwitchScreen.SetActive (true);
 			this.levelText.text = "Level " + (this.currentLevel + 1);
+			this.goalText.text = "Level " + (this.currentLevel + 1) + " goals: ";
+			this.finishedInit = true;
+			currentGameState = GameState.LEVEL_SWITCH;
 		}
 
 		private void UpdateLevelSwitch() {
 			// display level outline
 			briefingText.text = briefingTexts[this.currentLevel];
 			this.currentLevelObject = levels [this.currentLevel];
+			this.currentLevelObject.Init ();
 			if (Input.GetKeyUp ("space")) {
 				this.screenDimming.SetActive (false);
 				this.levelSwitchScreen.SetActive (false);
-				currentGameState = GameState.GAMEPLAY_STATE;
 				StartGamePlay ();
 			}
 		}
 
 		// ============================ Gameplay ==============================
 		private void StartGamePlay() {
+			this.finishedInit = false;
 			this.gameplayUI.SetActive (true);
+			this.goalList = null;
 			UpdateGoalList ();
+			UpdateUpNext ();
+			this.finishedInit = true;
+			currentGameState = GameState.GAMEPLAY_STATE;
 		}
 
 		private void UpdateGameplay() {
@@ -299,27 +357,34 @@ namespace jackyjjc {
 				SpawnBlock();
 			}
 
-			if (currentSpawnHeight - currentHighest <= 2.5 * blockSize) {
+			if (currentSpawnHeight - currentHighest <= 1.2f * blockSize) {
 				//need to scale up the spawn height
-				currentSpawnHeight += 1.5f * blockSize;
+				currentSpawnHeight += 1.2f * blockSize;
 				cameraTargetSize = currentSpawnHeight;
-			} else if (currentSpawnHeight - planetRadius - currentHighest >= 3.5f) {
+			} else if (currentHighest > float.Epsilon && currentSpawnHeight - currentHighest >= 2.2f) {
 				// need to scale down the spawn height
-				currentSpawnHeight -= 1.5f * blockSize;
+				currentSpawnHeight -= 1.2f * blockSize;
 				cameraTargetSize = currentSpawnHeight;
 			}
 		}
 
 		private void SpawnBlock() {
+			if (upNext == null) {
+				LevelEnd ();
+				return;
+			}
+
 			GameObject newBlock = Instantiate (blockPrefab, new Vector3(0, currentSpawnHeight, 0), Quaternion.identity);
 			newBlock.transform.RotateAround(Vector3.zero, Vector3.forward, Random.Range(0, 360));
 
-			BlockType blockType = blockTypes[Mathf.FloorToInt(Random.Range (0, blockTypes.Length))];
-			newBlock.GetComponent<Block> ().blockType = blockType;
-			newBlock.GetComponent<Rigidbody2D> ().mass = blockType.mass;
+			newBlock.GetComponent<Block> ().blockType = upNext;
+			newBlock.GetComponent<Rigidbody2D> ().mass = upNext.mass;
+			newBlock.GetComponent<SpriteRenderer> ().sprite = upNext.sprite;
 
 			playerControlBlock = newBlock;
 			landingSpeed = blockDefaultFallingSpeed;
+
+			UpdateUpNext ();
 		}
 
 		public void ActiveBlockCollision(Collision2D collision) {
@@ -329,29 +394,53 @@ namespace jackyjjc {
 			// Player cannot control this block anymore because it just landed
 			playerControlBlock = null;
 
-			Destroy(activeBlock.GetComponent<ActiveBlockCollider>());
-			activeBlock.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.None;
-			activeBlock.transform.SetParent (landedBlocks.transform);
+			bool landedSuccessfully = true;
+			bool isPerfect = false;
 
-			float height = CalculateBlockHeight (activeBlock);
-			if (height > currentHighest) {
-				currentHighest = height;
+			if (!collidee.name.Equals ("Planet")) {
+				Vector3 activeBlockDirection = (planet.transform.position - activeBlock.transform.position).normalized;
+				Vector3 activeBlockCollideeDirection = (activeBlock.transform.position - collidee.transform.position).normalized;
+				float angle = Vector3.Angle (-activeBlockDirection, activeBlockCollideeDirection);
+				landedSuccessfully = angle < MAX_ALLOWED_SIDEWAY_ANGLE;
+				if (landedSuccessfully) {
+					// rotate to the correct
+					Vector3 collideeDirection = (planet.transform.position - collidee.transform.position).normalized;
+					float correctAngle = Vector3.Angle (activeBlockDirection, collideeDirection);
+					if (correctAngle > 0 && correctAngle < 1) {
+						activeBlock.transform.RotateAround(planet.transform.position, Vector3.forward, -correctAngle);
+						isPerfect = true;
+					}
+				}
 			}
 
-			numBlocksLanded [activeBlock.GetComponent<Block> ().blockType] += 1;
+			if (landedSuccessfully) {
+				Destroy (activeBlock.GetComponent<ActiveBlockCollider> ());
+				activeBlock.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.None;
+				activeBlock.transform.SetParent (landedBlocks.transform);
 
-			// Update the level goal list
-			UpdateGoalList();
-
-			if (currentLevelObject.isAllGoalsSatisfied (this)) {
-				// Go to the next level
-				// If there is no next leve then win
-				if (currentLevel >= levels.Length - 1) {
-					// there is no more level, you win
-					Debug.Log("win");
-				} else {
-					// go to next level
+				float height = CalculateBlockHeight (activeBlock);
+				if (height > currentHighest) {
+					currentHighest = height;
 				}
+
+				BlockType blockType = activeBlock.GetComponent<Block> ().blockType;
+				numBlocksLanded [blockType] += 1;
+
+				int scoreMultiplier = 1;
+				if (isPerfect) {
+					scoreMultiplier++;
+					CreatePopupText (activeBlock.transform.position, activeBlock.transform.rotation, "Perfect! x2", blockType.color);
+				}
+				Score = Score + blockType.score * scoreMultiplier;
+
+				// Update the level goal list
+				UpdateGoalList ();
+
+				if (currentLevelObject.isAllGoalsSatisfied (this)) {
+					LevelEnd ();
+				}
+			} else {
+				DestroyBlock (activeBlock);
 			}
 		}
 
@@ -373,17 +462,100 @@ namespace jackyjjc {
 				}
 			}
 		}
+
+		private Text CreatePopupText(Vector3 gameLocation, Quaternion rotation, string text, Color color) {
+			Text popup = CreatePopupText (gameLocation, rotation, text);
+			popup.color = color;
+			return popup;
+		}
+
+		private Text CreatePopupText(Vector3 gameLocation, Quaternion rotation, string text) {
+			GameObject popUpText = Instantiate<GameObject> (popUpTextPrefab, this.popUpTextContainer.transform);
+			popUpText.transform.position = Camera.main.WorldToScreenPoint(gameLocation);
+			popUpText.transform.rotation = rotation;
+			popUpText.GetComponentInChildren<Text> ().text = text;
+			return popUpText.GetComponentInChildren<Text>();
+		}
+
+		private void DestroyBlock(GameObject block) {
+			BlockType blockType = block.GetComponent<Block> ().blockType;
+			numBlocksLanded [blockType] -= 1;
+			Score = Score - blockType.score;
+			Destroy (block);
+		}
+
+		private void UpdateUpNext() {
+			this.remainingText.text = currentLevelObject.GetNumRemainingBlocks() + " pods remaining\nUp next:";
+
+			upNext = currentLevelObject.getNextBlock (this);
+			if (upNext != null) {
+				infoPanel.SetActive (true);
+				upNextImage.sprite = upNext.sprite;
+				upNextDescription.text = upNext.name
+					+ (upNext.description.Length == 0 ? "" : "\n" + upNext.description)
+					+ "\nMass: " + upNext.mass
+					+ "\nScore: " + upNext.score;
+			} else {
+				infoPanel.SetActive (false);
+			}
+		}
+
+		private void LevelEnd() {
+			bool isPassed = currentLevelObject.isAllGoalsSatisfied (this);
+			if (isPassed) {
+				if (currentLevel >= levels.Length - 1) {
+					StartEndGame (true);
+				} else {
+					// TODO advance level;
+				}
+			} else {
+				// game ends
+				StartEndGame(false);
+			}
+		}
+
+		// ============================ endScreen ==============================
+		private void StartEndGame(bool isWon) {
+			this.finishedInit = false;
+			this.screenDimming.SetActive (true);
+			this.endGameScreen.SetActive (true);
+			this.titleText.text = (isWon ? "You've won!" : "Try Again!");
+			this.finalScoreText.text = "Your final score is: " + Score;
+			this.BodyText.text = "Thank you very much for spending time playing my game! Let me know your high score or any feedbacks you might have! You can comment on the itch.io page or @jackyjjc on Twitter.";
+			this.finishedInit = true;
+			currentGameState = GameState.GAME_END;
+		}
+
+		private void UpdateEndGame() {
+			if (Input.GetKeyUp ("space")) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+			} else if (Input.GetKeyUp ("s")) {
+				if (this.screenDimming.activeSelf) {
+					this.screenDimming.SetActive (false);
+					this.endGameScreen.SetActive (false);
+				} else {
+					this.screenDimming.SetActive (true);
+					this.endGameScreen.SetActive (true);
+				}
+			}
+		}
 	}
 
 	public class BlockType {
 		public readonly string name;
+		public readonly string description;
 		public readonly int mass;
+		public readonly int score;
 		public readonly Color color;
+		public readonly Sprite sprite;
 
-		public BlockType(string name, int mass, Color color) {
+		public BlockType(string name, int mass, int score, Color color, Sprite sprite, string description) {
 			this.name = name;
 			this.mass = mass;
 			this.color = color;
+			this.score = score;
+			this.sprite = sprite;
+			this.description = description;
 		}
 	}
 
@@ -392,14 +564,32 @@ namespace jackyjjc {
 		INTRO,
 		LEVEL_SWITCH,
 		GAMEPLAY_STATE,
-		WIN,
-		LOSS
+		GAME_END
 	}
 
 	public abstract class ILevel {
 		public abstract int numGoals ();
 		public abstract string[] renderGoalText(Game game);
 		public abstract bool isGoalSatisfied (Game game, int i);
+		protected abstract BlockType _getNextBlock (Game game);
+
+		private int _numRemaining = 0; 
+		public int GetNumRemainingBlocks () {
+			return _numRemaining;
+		}
+		protected void SetNumRemainingBlocks (int numRemaining) {
+			_numRemaining = numRemaining;
+		}
+
+		public BlockType getNextBlock (Game game) {
+			if (_numRemaining > 0) {
+				_numRemaining--;
+				return _getNextBlock (game);
+			}
+			return null;
+		}
+
+		public virtual void Init () {}
 
 		public bool isAllGoalsSatisfied(Game game) {
 			bool result = true;
@@ -414,21 +604,33 @@ namespace jackyjjc {
 	public class Level1 : ILevel {
 		public override string[] renderGoalText(Game game) {
 			return new string[] {
-				string.Format ("- Landed {0} / {1} pods", game.numBlocksLanded.Values.Sum (), 5)
+				string.Format ("- Land {0} / 5 pods", game.numBlocksLanded.Values.Sum ()),
+				string.Format ("- Score {0} >= 90", game.Score)
 			};
+		}
+
+		public override void Init() {
+			base.SetNumRemainingBlocks (10);
+		}
+
+		protected override BlockType _getNextBlock (Game game)
+		{
+			return game.blockTypes [Mathf.FloorToInt (Random.Range (0, 2))];
 		}
 
 		public override bool isGoalSatisfied(Game game, int index) {
 			switch (index) {
 			case 0:
-				return game.numBlocksLanded.Values.Sum () >= 5; 
+				return game.numBlocksLanded.Values.Sum () >= 5;
+			case 1:
+				return game.Score >= 90;
 			default:
 				return false;
 			}
 		}
 
 		public override int numGoals() {
-			return 1;
+			return 2;
 		}
 	}
 }
---
+++ Assets/Resources/Scripts/PopUpText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AnimatorClipInfo[] clipInfo = GetComponentInChildren<Animator> ().GetCurrentAnimatorClipInfo (0);
+		// Destroy the game object once the animation finishes
+		Destroy (gameObject, clipInfo [0].clip.length);
+	}
+}
